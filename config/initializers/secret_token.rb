7794dd2eadad9bba0c86305af0d4f8fd39475b2d
sajith-sahadevan <sajith.sahadevan@mobme.in>
2012-05-03 15:45:12 +0530

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoChat::Application.config.secret_token = '00bf2c9f8095a1bb58bc8c65ac7dae58edb3a3e045c6708337b0b01d622b107b20ad1c2f74f1b7033d0cb0f24cd2051f5c6e6b3778795d863963366287c7106c'
